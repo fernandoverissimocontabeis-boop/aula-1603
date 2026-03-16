e33f5c9ad5d475e206beb7c1603def1eb5833a75
fernandoverissimocontabeis-boop <fernandoverissimo.contabeis@gmail.com>
2026-03-16 20:15:48 -0300

    calculei a media

---
+++ calculo de média.sql
@@ -0,0 +1,10 @@
+Inicio
+   nota1 --digite a 1ª nota do aluno
+   nota2 --digite a 2ª nota do aluno
+   média (nota1+ nota2)/2 -- a média das notas do aluno
+   se média >=7 então
+    escreva "Parabéns, aprovado"
+   senão
+    escreva "Parabéns, volte ano que vem"
+   fim-se
+fim
