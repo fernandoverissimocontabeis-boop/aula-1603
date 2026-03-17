15464aa59ee9502ae2b72e449a45728c5aa61d40
fernandoverissimocontabeis-boop <fernandoverissimo.contabeis@gmail.com>
2026-03-16 21:47:28 -0300

    calculei a media script

--- media_teste.js
+++ media_teste.js
@@ -1,14 +1,3 @@
-Inicio
-   nota1 --digite a 1ª nota do aluno
-   nota2 --digite a 2ª nota do aluno
-   média (nota1+ nota2)/2 -- a média das notas do aluno
-   se média >=7 então
-    escreva "Parabéns, aprovado"
-   senão
-    escreva "Parabéns, volte ano que vem"
-   fim-se
-fim
-
 const readline = require("readline");
 
 const rl = readline.createInterface({
